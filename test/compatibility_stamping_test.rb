5705f523b0c1d9970712d6305a546bb9ab838e88
Thomas von Deyen <thomas@vondeyen.com>
2020-07-31 09:43:10 +0200

    Remove duplicated test

--- test/compatibility_stamping_test.rb
+++ test/compatibility_stamping_test.rb
@@ -20,17 +20,6 @@ class CompatibilityStampingTests < Test::Unit::TestCase # :nodoc:
     assert_equal @nicole, comment.updater
   end
 
-  def test_comment_creation_with_stamped_integer
-    Person.stamper = @nicole.id
-    assert_equal @nicole.id, Person.stamper
-
-    comment = Comment.create(:comment => "Test Comment - 2")
-    assert_equal @nicole.id, comment.created_by
-    assert_equal @nicole.id, comment.updated_by
-    assert_equal @nicole, comment.creator
-    assert_equal @nicole, comment.updater
-  end
-
   def test_comment_creation_with_stamped_object
     assert_equal @delynn.id, Person.stamper
 
